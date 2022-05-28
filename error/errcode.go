aa18822a51bc2e196d1cb84a46605c58acc83c0b
xinzwang <1790828743@qq.com>
2022-05-29 02:04:22 +0800

    调整

--- error/errcode.go
+++ error/errcode.go
@@ -2,13 +2,20 @@ package error
 
 const (
 	// 用户错误
-	NOT_LOGIN   = 10001 // 未登录
-	FAILED_AUTH = 10002 // 验证身份失败
-	FAILED_REG  = 10003 // 注册失败
+	FAILED_NO_TOKEN      = 10001 // 无token
+	FAILED_INVALID_TOKEN = 10002 // 无效的token
+
+	FAILED_AUTH = 10003 // 验证身份失败
+	FAILED_REG  = 10004 // 注册失败
+
+	ERR_NOT_AUTH     = 10100 // 未通过身份验证
+	ERR_ALREADY_AUTH = 10101 //已经通过身份验证
 
 	// 应用层错误
 	ERR_REQ_DATA     = 20001 // 请求数据结构错误
 	ERR_DB_NOT_FOUND = 20002 // 数据库无对应数据
+
+	FAILED_USER_NOT_FOUND = 20003 // 用户尚未录入系统
 
 	// 服务层错误
 
